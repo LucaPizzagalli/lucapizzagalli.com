65cd2d3af2de6fc3c3f681a30bd7e28254085941
LucaPizzagalli <lucapizzagalli@gmail.com>
2019-09-23 14:11:17 +0200

    added github links

--- src/components/header.js
+++ src/components/header.js
@@ -23,7 +23,7 @@ function Header() {
     <AppBar color="primary" position="static">
       <Toolbar>
         <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-          <svg class="MuiSvgIcon-root" focusable="false" viewBox="0 0 24 24" aria-hidden="true" role="presentation" width="1.2em"><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path></svg>
+          <svg focusable="false" viewBox="0 0 24 24" aria-hidden="true" role="presentation" width="1.2em"><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path></svg>
         </IconButton>
         <Typography variant="h6" className={classes.title}>
           <Link to="/" activeStyle={{ color: `black` }}
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -12,24 +12,30 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Chip from '@material-ui/core/Chip';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
 import Typography from '@material-ui/core/Typography';
 
 import opociiCover from "../images/opociiCover.png"
-
+import microswimmersCover from "../images/microswimmersCover.png"
+import chemotaxisCover from "../images/chemotaxisCover.png"
+import memoryCardsCover from "../images/memoryCardsCover.png"
 
 const previews = [
 
   {
-    title: "Opocii Simulation",
-    imageDir: "/projects/opociiSimulation",
+    title: "Opocii world",
+    link: "/projects/opocii",
+    image: opociiCover,
     imageAlt: "Contemplative Reptile",
     imageTitle: "cosa",
     excerpt: "Opocii are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
-    technologies: ["C"]
+    technologies: ["C"],
   },
   {
-    title: "Unreal Engine Prototype",
-    imageDir: "/projects/opociiSimulation",
+    title: "Unreal Engine 4 prototype",
+    link: "/projects/ue4",
+    image: opociiCover,
     imageAlt: "Contemplative Reptile",
     imageTitle: "cosa",
     excerpt: "Opocii are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
@@ -37,19 +43,40 @@ const previews = [
   },
   {
     title: "Micro-swimmers simulation",
-    imageDir: "/projects/opociiSimulation",
-    imageAlt: "Contemplative Reptile",
-    imageTitle: "cosa",
+    link: "/static/microswimmersSlides/slides.html",
+    image: microswimmersCover,
+    imageAlt: "comparison experiment simulation",
+    imageTitle: "comparison experiment simulation",
     excerpt: "Opocii are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
-    technologies: ["C++"]
+    technologies: ["C++", "Python"],
+    github: "https://github.com/LucaPizzagalli/motion-microswimmers-simulation"
   },
   {
     title: "This same website",
-    imageDir: "/projects/opociiSimulation",
+    image: opociiCover,
     imageAlt: "Contemplative Reptile",
     imageTitle: "cosa",
     excerpt: "Opocii are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
-    technologies: ["Html", "Javascript", "primary"]
+    technologies: ["Html", "Javascript", "React"],
+    github: "https://github.com/LucaPizzagalli/LucaPizzagalli.local"
+  },
+  {
+    title: "Chemotaxis simulation",
+    image: chemotaxisCover,
+    imageAlt: "Contemplative Reptile",
+    imageTitle: "cosa",
+    excerpt: "Opocii are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+    technologies: ["C++"],
+    github: "https://github.com/LucaPizzagalli/chemotaxis-pathfinding"
+  },
+  {
+    title: "Memory Cards",
+    image: memoryCardsCover,
+    imageAlt: "Contemplative Reptile",
+    imageTitle: "cosa",
+    excerpt: "Opocii are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+    technologies: ["Python", "SQL"],
+    github: "https://github.com/LucaPizzagalli/memory-cards"
   },
 ]
 
@@ -63,12 +90,12 @@ const Portfolio = () => (
         <Grid item sm={12} md={6}>
           <Card>{/* className={classes.card}>*/}
             <CardActionArea>
-              <Link to="/projects/opociiSimulation" style={{ textDecoration: `none`, }}>
+              <Link to={preview.link} style={{ textDecoration: `none`, }}>
                 <CardMedia
                   component="img"
                   alt={preview.imageAlt}
                   height="200"
-                  image={opociiCover}
+                  image={preview.image}
                   title="Opocii"
                 />
                 <CardContent>
@@ -79,6 +106,18 @@ const Portfolio = () => (
             </CardActionArea>
             <CardActions>
               {preview.technologies.map(tech => (<Chip label={tech} clickable color="primary" />))}
+              {(() => {
+                if (preview.github)
+                {
+                return (<Tooltip title="View the source on Github" placement="top" style={{ marginLeft: 'auto', }}>
+                  <IconButton href={preview.github}>
+                    <svg focusable="false" viewBox="0 0 24 24" aria-hidden="true" role="presentation" fill="grey" width="1em">
+                      <path d="M12 .3a12 12 0 0 0-3.8 23.4c.6.1.8-.3.8-.6v-2c-3.3.7-4-1.6-4-1.6-.6-1.4-1.4-1.8-1.4-1.8-1-.7.1-.7.1-.7 1.2 0 1.9 1.2 1.9 1.2 1 1.8 2.8 1.3 3.5 1 0-.8.4-1.3.7-1.6-2.7-.3-5.5-1.3-5.5-6 0-1.2.5-2.3 1.3-3.1-.2-.4-.6-1.6 0-3.2 0 0 1-.3 3.4 1.2a11.5 11.5 0 0 1 6 0c2.3-1.5 3.3-1.2 3.3-1.2.6 1.6.2 2.8 0 3.2.9.8 1.3 1.9 1.3 3.2 0 4.6-2.8 5.6-5.5 5.9.5.4.9 1 .9 2.2v3.3c0 .3.1.7.8.6A12 12 0 0 0 12 .3"></path>
+                    </svg>
+                  </IconButton>
+                </Tooltip>);
+                }
+              })()}
             </CardActions>
           </Card>
         </Grid>
