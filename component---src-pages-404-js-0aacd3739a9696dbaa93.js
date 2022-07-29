04c319ae9d8a07c6798e5c181325a3126991db63
LucaPizzagalli <lucapizzagalli@gmail.com>
2022-07-29 23:46:08 +0200

    Updates

---
+++ component---src-pages-404-js-0aacd3739a9696dbaa93.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkluca_pizzagalli_website=self.webpackChunkluca_pizzagalli_website||[]).push([[883],{429:function(e,t,l){l.r(t);var n=l(7294),a=l(3837),u=l(262);t.default=function(){return n.createElement(a.Z,null,n.createElement(u.Z,{title:"404: Not found",description:"",lang:"en",meta:[]}),n.createElement("h1",null,"Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-0aacd3739a9696dbaa93.js.map
