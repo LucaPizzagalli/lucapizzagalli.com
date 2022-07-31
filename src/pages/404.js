3671e9b5b85f017d04188deaf11e5c5c4b20be9b
LucaPizzagalli <mail@lucapizzagalli.com>
2022-07-31 23:31:26 +0200

    bugfix

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,7 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 const NotFoundPage = () => (
-    <Layout>
+    <Layout location="/404">
         <Seo title="404: Not found" description="" lang="en" meta={[]} />
         <h1>Not Found</h1>
         <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
