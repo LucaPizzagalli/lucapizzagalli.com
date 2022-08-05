a212168013e47c721079eb2b1d69e7179ab22a20
LucaPizzagalli <mail@lucapizzagalli.com>
2022-08-05 17:44:30 +0200

    updated images and added analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,14 @@
 module.exports = {
   siteMetadata: {
+    siteUrl: `https://lucapizzagalli.com`,
     title: `Luca Pizzagalli website`,
     description: `Luca Pizzagalli personal website`,
     author: `Luca Pizzagalli`,
   },
   plugins: [
+    // Header
     `gatsby-plugin-react-helmet`,
+    // Sourcing files from filesystem
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -27,6 +30,22 @@ module.exports = {
         path: `${__dirname}/src/blog/`,
       },
     },
+    // Images
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `luca-pizzagalli-personal-website`,
+        short_name: `luca-pizzagalli`,
+        start_url: `/`,
+        background_color: `#111111`,
+        theme_color: `#111111`,
+        display: `minimal-ui`,
+        icon: `src/assets/ico.png`,
+      },
+    },
+    // Markdown
     {
       resolve: `gatsby-transformer-remark`,
       options: {
@@ -40,7 +59,7 @@ module.exports = {
           },
           {
             resolve: `gatsby-remark-images`,
-            options: {maxWidth: 2000, showCaptions: true},
+            options: { maxWidth: 2000, showCaptions: true },
           },
         ],
       },
@@ -64,20 +83,33 @@ module.exports = {
         },
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-image`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: `gatsby-plugin-feed`,
       options: {
-        name: `luca-pizzagalli-personal-website`,
-        short_name: `luca-pizzagalli`,
-        start_url: `/`,
-        background_color: `#111111`,
-        theme_color: `#111111`,
-        display: `minimal-ui`,
-        icon: `src/assets/ico.png`,
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [],
       },
-    }
+    },
+    {
+      resolve: `gatsby-plugin-goatcounter`,
+      options: {
+        code: 'lucapizzagalli',
+
+        // REQUIRED IF USING SELFHOSTED GOATCOUNTER!
+        // selfHostUrl: `https://example.com`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
