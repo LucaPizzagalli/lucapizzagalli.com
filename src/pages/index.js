bfff5ce52c13849f3bc61f2bd65c190c0066f47e
LucaPizzagalli <mail@lucapizzagalli.com>
2023-07-01 17:34:04 +0200

    formatting

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,20 +5,24 @@ import Seo from "../components/seo";
 
 
 function IndexPage({ loop }) {
-    return (
-        <Layout loop={loop} location="/">
-            <Seo title="Luca Pizzagalli" description="" lang="en" meta={[]} />
-            <h1>Hello.</h1>
-            <div>
-                <div>
-                    <p>I'm Luca.</p>
-                    <p>
-                        The place you are now in is the website I put together in a bunch of days for declaring to the world that <em style={{ textDecoration: "line-through" }}>I'm looking for a job</em> <strong>I found a job</strong>.
-                        A job in <strong>data science</strong>, or something related. Every place in <em style={{ textDecoration: "line-through" }}>the world</em> <strong>Berlin</strong> is fine.</p>
-                </div>
-            </div>
-        </Layout>
-    );
+  return (
+    <Layout loop={loop} location="/">
+      <Seo title="Luca Pizzagalli" description="" lang="en" meta={[]} />
+      <h1>Hello.</h1>
+      <div>
+        <div>
+          <p>I'm Luca.</p>
+          <p>
+            The place you are now in is the website I put together in a bunch of days for declaring to the world that
+            <em style={{ textDecoration: "line-through" }}>I'm looking for a job</em> <strong>I found a job</strong>.
+            A job in <strong>data science</strong>, or something related.
+            Every place in <em style={{ textDecoration: "line-through" }}>the world</em> <strong>Berlin</strong> is fine.
+            {/* Make yourself comfortable, grab a chair, grab a drink. */}
+          </p>
+        </div>
+      </div>
+    </Layout>
+  );
 }
 
 export default IndexPage
